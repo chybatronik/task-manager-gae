0fbe1e81ebf3fadc376b29a175bc793198f6074c
chybatronik <chybatron@gmail.com>
2013-05-30 16:05:10 +0400

    test Company model

--- models.py
+++ models.py
@@ -1,4 +1,10 @@
 from google.appengine.ext import db
 
+class Company(db.Model):
+	name = db.StringProperty(required=True)
+	date = db.DateTimeProperty(auto_now_add=True)
+	create_by = db.UserProperty(required=True)
+
+
 class TestModel(db.Model):
-      pass
+    pass
--- test/test_mainn.py
+++ test/test_mainn.py
@@ -1,11 +1,13 @@
 import unittest
 from google.appengine.ext import db
+from google.appengine.api.users import User
 from google.appengine.ext import testbed
 from google.appengine.datastore import datastore_stub_util
 
 from main import *
+from datetime import datetime
 
-class DemoTestCase(unittest.TestCase):
+class CompanyTestCase(unittest.TestCase):
 
   def setUp(self):
     # First, create an instance of the Testbed class.
@@ -13,24 +15,41 @@ class DemoTestCase(unittest.TestCase):
     # Then activate the testbed, which prepares the service stubs for use.
     self.testbed.activate()
     # Create a consistency policy that will simulate the High Replication consistency model.
-    self.policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=0)
+    #self.policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=0)
     # Initialize the datastore stub with this policy.
-    self.testbed.init_datastore_v3_stub(consistency_policy=self.policy)
+    self.testbed.init_datastore_v3_stub()
+    self.testbed.init_memcache_stub()
 
   def tearDown(self):
     self.testbed.deactivate()
 
-  def testEventuallyConsistentGlobalQueryResult(self):
-    
+  def testCompany(self):
+    user = User(email = "test@foo.com")
+    company1 = Company(name = "Name", create_by= user)
+    company1.put()
+    company2 = Company(name = "Name", create_by= user)
+    company2.put()
 
-    user_key = db.Key.from_path('User', 'ryan')
-    # Put two entities
-    db.put([TestModel(parent=user_key), TestModel(parent=user_key)])
+    self.assertEqual(2, len(Company.all().fetch(2)))
 
-    # Global query doesn't see the data.
-    self.assertEqual(0, TestModel.all().count(3))
-    # Ancestor query does see the data.
-    self.assertEqual(2, TestModel.all().ancestor(user_key).count(3))
+  def test_without_name_Company(self):  
+    user = User(email = "test@foo.com")
+    self.assertRaises(db.BadValueError, Company, create_by= user)
+
+  def test_without_user_Company(self):    
+    self.assertRaises(db.BadValueError, Company, name="asd")
+
+  def test_with_user_Company(self):
+    user = User(email = "test@foo.com")
+    Company(name = "qwe", create_by= user).put()
+
+    self.assertEqual(1, len(Company.all().fetch(2)))
+
+  def test_datetime_Company(self):
+    user = User(email = "test@foo.com")
+    Company(name = "qwe", create_by= user).put()
+    self.assertTrue(Company.date)
+
 
 if __name__ == '__main__':
     unittest.main()
