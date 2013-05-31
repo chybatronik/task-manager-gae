a6c834d0db694ea3ddf413c200d6a4c56ab3d8a9
chybatronik <chybatron@gmail.com>
2013-05-31 14:33:01 +0400

    add validator email

--- models.py
+++ models.py
@@ -1,12 +1,33 @@
+from re import match, IGNORECASE, UNICODE, VERBOSE
 from google.appengine.ext import db
 from google.appengine.api.users import User
+
+def valid_email(email):
+    if not email:
+        raise db.BadValueError('Please enter an email address')
+
+    if len(email) > 75:
+        raise db.BadValueError('Email address must be 6-75 characters long')
+
+    if not match(r'''
+        ^
+        [\w.%+-]{1,75}
+        @
+        [a-z\d.-]{1,75}
+        \.
+        [a-z]{2,4}
+        $
+        ''', email, IGNORECASE | VERBOSE):
+        raise db.BadValueError('Invalid email address')
 
 class Company(db.Model):
     name = db.StringProperty(required=True)
     date = db.DateTimeProperty(auto_now_add=True)
-    email = db.EmailProperty(required=True)
+    email = db.EmailProperty(required=True, validator=valid_email)
     atach_users = db.ListProperty(User)
     create_by = db.UserProperty(required=True)
+
+    
 
 
 class Task(db.Model):
