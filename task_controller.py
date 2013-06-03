0a623034f82aaae9ee12917de433237c44c99347
chybatronik <chybatron@gmail.com>
2013-06-03 08:29:35 +0400

    add validator create edit task model

--- task_controller.py
+++ task_controller.py
@@ -16,20 +16,24 @@ class ShowTaskPage(BaseRequestHandler):
         self.generate("task/show.html", template_values)
 
 class CreateTaskPage(BaseRequestHandler):
-    def get (self):
-    	template_values = {
-            "companies":Company.all()
+    def get (self, errors=None):
+        template_values = {
+            "companies":Company.all(),
+            "errors":errors,
         }
         self.generate("task/new.html", template_values)
 
     def post(self):
-    	company = Company.get_by_id(int(self.request.get("company")))
-        task = Task(title = self.request.get("title"), text = self.request.get("text"), 
-            create_by = users.get_current_user(), company = company).put()
-        self.redirect("/tasks/" + str(task.id()))  
+        try:
+            company = Company.get_by_id(int(self.request.get("company")))
+            task = Task(title = self.request.get("title"), text = self.request.get("text"), 
+                create_by = users.get_current_user(), company = company).put()
+            self.redirect("/tasks/" + str(task.id()))  
+        except db.BadValueError, errors:
+            self.get(errors)
 
 class EditTaskPage(BaseRequestHandler):
-    def get(self, task_id):
+    def get(self, task_id, errors=None):
         template_values = {
             "task":Task.get_by_id(int(task_id)),
             "companies":Company.all(),
@@ -37,14 +41,16 @@ class EditTaskPage(BaseRequestHandler):
         self.generate("task/edit.html", template_values)
 
     def post(self, task_id):
-    	company = Company.get_by_id(int(self.request.get("company")))
-        task = Task.get_by_id(int(task_id))
-        task.title = self.request.get("title")
-        task.text = self.request.get("text")
-        task.company = company
-        task.put()
-        self.redirect("/tasks/" + str(task.key().id())) 
-
+        try:
+            company = Company.get_by_id(int(self.request.get("company")))
+            task = Task.get_by_id(int(task_id))
+            task.title = self.request.get("title")
+            task.text = self.request.get("text")
+            task.company = company
+            task.put()
+            self.redirect("/tasks/" + str(task.key().id())) 
+        except db.BadValueError, errors:
+            self.get(int(task_id), errors)
 
 class DeleteTaskPage(BaseRequestHandler):
     def post(self, task_id):
